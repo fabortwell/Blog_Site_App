817c45f31e305cefeadba17cb1a694fa8e493ed5
user <tre@Brian.local>
2022-08-04 14:44:30 +0300

    aunthetication

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_03_162221) do
+ActiveRecord::Schema.define(version: 2022_08_04_102817) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,9 +57,9 @@ ActiveRecord::Schema.define(version: 2022_08_03_162221) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
-    t.string "password_digest"
-    t.string "image_url"
     t.string "bio"
+    t.string "password_digest"
+    t.string "avatar"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
